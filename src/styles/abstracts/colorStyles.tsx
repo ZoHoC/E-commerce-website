c9ec5357aac7ed86d299fb9d9bd37bfd904e8115
ZoHoC <fajrunt.tw@gmail.com>
2023-07-24 17:51:25 +0200

    abstract styles

---
+++ src/styles/abstracts/colorStyles.tsx
@@ -0,0 +1,13 @@
+export default {
+  primaryColor: "#1d1d1b",
+  secondaryColor: "#1e71bb",
+  tertiaryColor: "#979797",
+  accentColor: "#bb1e1e",
+  lightColor: "#ffffff",
+  primaryBgColor: "#f2f2f2",
+  secondaryBgColor: "#ffffff",
+  tertiaryBgColor: "#8d8d8c",
+  quaternaryBgColor: "#F4F5F8",
+  accentBgColor: "#bb1e1e",
+  accentBgGradientColor: "#b21919",
+};
