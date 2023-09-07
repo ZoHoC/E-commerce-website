713715e8b4d47e99173f54fc8a1b9c5cd1a65538
ZoHoC <luxsgw@gmail.com>
2023-09-07 17:02:45 +0200

    Updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,35 @@
+import Contact from "@/components/Contact/Contact";
 import styles from "./page.module.scss";
 import Footer from "@/components/Footer/Footer";
+import Landing from "@/components/Landing/Landing";
+import Section from "@/components/Section/Section";
 import Header from "@/modules/Header/Header";
+import Services from "@/components/Services/Services";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Landing />
+        <Section
+          title={"Featured Products"}
+          isTertiary
+          titleAlignment="center"
+        ></Section>
+        <Section
+          title={"Custom Furniture Built Only For You"}
+          actionContainer={
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quisquam saepe id reiciendis sunt, repudiandae libero amet rem quia quod?"
+          }
+          isSecondary
+        >
+          <Services />
+        </Section>
+        <Section title={"Join our newsletter and get 20% off"}>
+          <Contact />
+        </Section>
+      </main>
       <Footer />
     </>
   );
