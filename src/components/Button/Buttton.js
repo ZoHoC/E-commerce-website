b730ca8727a2571aa05bed29abf687fb2ad35577
ZoHoC <luxsgw@gmail.com>
2023-09-07 16:52:28 +0200

    Button rework

--- src/components/Button/Buttton.js
+++ src/components/Button/Buttton.js
@@ -1,19 +1,11 @@
 import styles from "./Button.module.scss";
 
-const Button = ({
-  isSecondary = false,
-  isTertiary = false,
-  handleClick,
-  children,
-}) => {
+const Button = ({ isSecondary = false, handleClick, children }) => {
   let modifierClass = styles["Button"];
 
   switch (true) {
     case isSecondary:
       modifierClass = `${styles["Button"]} ${styles["Button_secondary"]}`;
-      break;
-    case isTertiary:
-      modifierClass = `${styles["Button"]} ${styles["Button_tertiary"]}`;
       break;
     default:
       modifierClass;
