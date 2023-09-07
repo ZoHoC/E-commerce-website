d50cf1124711b5e533547e185fac5eea7ed8e30c
ZoHoC <luxsgw@gmail.com>
2023-09-07 16:53:53 +0200

    Added service data

---
+++ src/utility/serviceData.js
@@ -0,0 +1,24 @@
+import Compass from "@/../public/assets/icon/Compass.svg";
+import Diamond from "@/../public/assets/icon/Diamond.svg";
+import StabbedNote from "@/../public/assets/icon/StabbedNote.svg";
+
+export const services = [
+  {
+    id: 1,
+    icon: <Compass />,
+    title: "mission",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 2,
+    icon: <Diamond />,
+    title: "vision",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+  {
+    id: 3,
+    icon: <StabbedNote />,
+    title: "history",
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+  },
+];
