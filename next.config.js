c9ed4335987a4e303af80ede12f49add49d590f0
ZoHoC <fajrunt.tw@gmail.com>
2023-09-10 22:55:09 +0200

    Images allowance

--- next.config.js
+++ next.config.js
@@ -6,12 +6,24 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+
   webpack: config => {
     config.module.rules.push({
       test: /\.svg$/i,
       use: ["@svgr/webpack"],
     });
     return config;
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.course-api.com",
+        port: "",
+        pathname: "/images/store/**",
+      },
+    ],
   },
 };
 
