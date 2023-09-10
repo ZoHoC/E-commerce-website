3410ef5697e0ece64a8303c311516c703eb6d77a
ZoHoC <fajrunt.tw@gmail.com>
2023-09-10 22:57:42 +0200

    Added Featured Products module

---
+++ src/modules/FeaturedProducts/FeaturedProducts.js
@@ -0,0 +1,30 @@
+import styles from "./FeaturedProducts.module.scss";
+
+import Button from "../../components/Button/Buttton";
+import { fetchData } from "@/api/productsFetch";
+import Product from "@/components/Product/Product";
+import randomizeArray from "@/utility/randomizeArray";
+
+const FeaturedProducts = async () => {
+  const data = await fetchData();
+  const shuffledData = randomizeArray(data);
+
+  return (
+    <div className={styles["FeaturedProducts"]}>
+      <div className={styles["FeaturedProducts-Container"]}>
+        <h2 className={styles["FeaturedProducts-Title"]}>featured products</h2>
+        <div className={styles["FeaturedProducts-Underline"]}></div>
+      </div>
+      <div className={styles["FeaturedProducts-Grid"]}>
+        {shuffledData.slice(0, 3).map(({ id, name, image, price }) => {
+          return <Product key={id} name={name} image={image} price={price} />;
+        })}
+      </div>
+      <div className={styles["FeaturedProducts-Button"]}>
+        <Button>all products</Button>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
