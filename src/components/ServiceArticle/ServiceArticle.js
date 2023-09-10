1314085b47ab8a0172bf4f8b157c87242a6c872b
ZoHoC <fajrunt.tw@gmail.com>
2023-09-10 22:57:20 +0200

    Removed uneccessery key attribute

--- src/components/ServiceArticle/ServiceArticle.js
+++ src/components/ServiceArticle/ServiceArticle.js
@@ -1,8 +1,8 @@
 import styles from "./ServiceArticle.module.scss";
 
-const ServiceArticle = ({ uniqueKey, icon, title, text }) => {
+const ServiceArticle = ({ icon, title, text }) => {
   return (
-    <article key={uniqueKey} className={styles["ServiceArticle"]}>
+    <article className={styles["ServiceArticle"]}>
       <div className={styles["ServiceArticle-Icon"]}>{icon}</div>
       <h3 className={styles["ServiceArticle-Title"]}>{title}</h3>
       <p className={styles["ServiceArticle-Description"]}>{text}</p>
