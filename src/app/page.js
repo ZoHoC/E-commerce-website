ea99ce0b60ffcca13bad67a7f82ab70fbfc093dc
ZoHoC <fajrunt.tw@gmail.com>
2023-09-11 22:32:25 +0200

    Added Homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,3 @@
-import styles from "./page.module.scss";
-
 import Contact from "@/components/Contact/Contact";
 import Footer from "@/components/Footer/Footer";
 import Landing from "@/components/Landing/Landing";
