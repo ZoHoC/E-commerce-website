51f903283dec447fa0bd7ba04b906085f5a2c7f3
ZoHoC <fajrunt.tw@gmail.com>
2023-09-11 22:33:42 +0200

    Added pathname to highlight which page user is on

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,46 @@
-import Link from "next/link";
 import styles from "./Navbar.module.scss";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import CartIcon from "../../../public/assets/icon/CartIcon.svg";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
+  const isCurrentPage = href => pathname === href;
+
   return (
     <nav className={styles["Navbar"]}>
-      <Link className={styles["Navbar-Link"]} href="/">
+      <Link
+        className={`${styles["Navbar-Link"]} ${
+          isCurrentPage("/") ? styles["Navbar-Link_current"] : ""
+        }`}
+        href="/"
+      >
         Home
       </Link>
-      <Link className={styles["Navbar-Link"]} href="/about">
+      <Link
+        className={`${styles["Navbar-Link"]} ${
+          isCurrentPage("/about") ? styles["Navbar-Link_current"] : ""
+        }`}
+        href="/about"
+      >
         About
       </Link>
-      <Link className={styles["Navbar-Link"]} href="/products">
+      <Link
+        className={`${styles["Navbar-Link"]} ${
+          isCurrentPage("/products") ? styles["Navbar-Link_current"] : ""
+        }`}
+        href="/products"
+      >
         Products
       </Link>
-      <Link className={styles["Navbar-Cart"]} href="/cart">
+      <Link
+        className={`${styles["Navbar-Cart"]} ${
+          isCurrentPage("/cart") ? styles["Navbar-Link_current"] : ""
+        }`}
+        href="/cart"
+      >
         Cart
         <div className={styles["Navbar-Icon"]}>
           <CartIcon />
