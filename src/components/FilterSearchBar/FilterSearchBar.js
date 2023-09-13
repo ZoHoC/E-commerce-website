edd9068dab5748ba182c2c0e8c05b27e3e1f2f17
ZoHoC <fajrunt.tw@gmail.com>
2023-09-14 00:03:47 +0200

    added filter searchbar component

---
+++ src/components/FilterSearchBar/FilterSearchBar.js
@@ -0,0 +1,19 @@
+import { useDispatch } from "react-redux";
+import styles from "./FilterSearchBar.module.scss";
+
+const FilterSearchBar = ({ text, updateFilters }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <input
+      type="text"
+      name="text"
+      placeholder="search"
+      className={styles["FilterSearchBar"]}
+      value={text}
+      onChange={e => dispatch(updateFilters({ text: e.target.value }))}
+    />
+  );
+};
+
+export default FilterSearchBar;
