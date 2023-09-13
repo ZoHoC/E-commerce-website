1dc59b6129305f99e57fcf939d456bda18896ce9
ZoHoC <fajrunt.tw@gmail.com>
2023-09-14 00:02:43 +0200

    Added tertiary style

--- src/components/Button/Buttton.js
+++ src/components/Button/Buttton.js
@@ -1,11 +1,19 @@
 import styles from "./Button.module.scss";
 
-const Button = ({ isSecondary = false, handleClick, children }) => {
+const Button = ({
+  isSecondary = false,
+  isTertiary = false,
+  handleClick,
+  children,
+}) => {
   let modifierClass = styles["Button"];
 
   switch (true) {
     case isSecondary:
       modifierClass = `${styles["Button"]} ${styles["Button_secondary"]}`;
+      break;
+    case isTertiary:
+      modifierClass = `${styles["Button"]} ${styles["Button_tertiary"]}`;
       break;
     default:
       modifierClass;
