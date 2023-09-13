72f2c528902889111c53d2f42b1e7775b7793626
ZoHoC <fajrunt.tw@gmail.com>
2023-09-14 00:01:24 +0200

    Redux provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import "@/styles/globals.scss";
 
+import Providers from "@/redux/provider";
+
 import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
@@ -15,7 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
