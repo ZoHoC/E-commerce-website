e5590e4e281ec0b24b7416f32bf80faf20b6a38c
ZoHoC <fajrunt.tw@gmail.com>
2023-09-17 21:29:22 +0200

    Data provided thru fetch instead of redux Selector

--- src/modules/ProductGrid/ProductGrid.js
+++ src/modules/ProductGrid/ProductGrid.js
@@ -1,14 +1,34 @@
 "use client";
-import SortingBar from "@/components/SortingBar/SortingBar";
-import Filters from "../Filters/Filters";
-import styles from "./ProductGrid.module.scss";
-import ProductCard from "@/components/ProductCard/ProductCard";
-import { useSelector } from "react-redux";
-import { getFilteredProducts } from "@/redux/selector/getFilteredProducts";
 
-const ProductGrid = () => {
-  const { view } = useSelector(state => state.filters);
-  const filteredProducts = useSelector(getFilteredProducts);
+import styles from "./ProductGrid.module.scss";
+
+import Filters from "../Filters/Filters";
+import SortingBar from "@/components/SortingBar/SortingBar";
+import ProductCard from "@/components/ProductCard/ProductCard";
+
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { filterAndSortProducts } from "@/utility/filterAndSortProducts";
+
+const ProductGrid = ({ data }) => {
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const { text, category, company, color, price, shipping, sortBy, view } =
+    useSelector(state => state.filters);
+
+  useEffect(() => {
+    const filtered = filterAndSortProducts(data, {
+      text,
+      category,
+      company,
+      color,
+      price,
+      shipping,
+      sortBy,
+      view,
+    });
+
+    setFilteredProducts(filtered);
+  }, [text, category, company, color, price, shipping, sortBy, view]);
 
   const gridClass = view
     ? styles["ProductGrid-Grid"]
@@ -16,10 +36,10 @@ const ProductGrid = () => {
 
   return (
     <div className={styles["ProductGrid"]}>
-      <Filters />
+      <Filters data={data} />
       <div className={gridClass}>
         <div className={styles["ProductGrid-SortingBar"]}>
-          <SortingBar />
+          <SortingBar filteredProducts={filteredProducts} />
         </div>
         {filteredProducts.length === 0 && (
           <div className={styles["ProductGrid-NoProducts"]}>
@@ -43,3 +63,50 @@ const ProductGrid = () => {
 };
 
 export default ProductGrid;
+
+// ------------------------------------------------------------------------------------------------------------------------------
+
+// import SortingBar from "@/components/SortingBar/SortingBar";
+// import Filters from "../Filters/Filters";
+// import styles from "./ProductGrid.module.scss";
+// import ProductCard from "@/components/ProductCard/ProductCard";
+// import { useSelector } from "react-redux";
+// import { getFilteredProducts } from "@/redux/selector/getFilteredProducts";
+
+// const ProductGrid = () => {
+//   const { view } = useSelector(state => state.filters);
+//   const filteredProducts = useSelector(getFilteredProducts);
+
+//   const gridClass = view
+//     ? styles["ProductGrid-Grid"]
+//     : styles["ProductGrid-List"];
+
+//   return (
+//     <div className={styles["ProductGrid"]}>
+//       <Filters />
+//       <div className={gridClass}>
+//         <div className={styles["ProductGrid-SortingBar"]}>
+//           <SortingBar />
+//         </div>
+//         {filteredProducts.length === 0 && (
+//           <div className={styles["ProductGrid-NoProducts"]}>
+//             Sorry, no products matched your search...
+//           </div>
+//         )}
+//         {filteredProducts.map(({ id, name, price, description, image }) => (
+//           <ProductCard
+//             key={id}
+//             id={id}
+//             name={name}
+//             price={price}
+//             description={description}
+//             image={image}
+//             view={view}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProductGrid;
