aea288cb42814de1d5d6ec04842c08854ba65364
ZoHoC <fajrunt.tw@gmail.com>
2023-09-26 16:24:25 +0200

    Created cart page

---
+++ src/app/cart/page.js
@@ -0,0 +1,14 @@
+import Footer from "@/components/Footer/Footer";
+import Header from "@/modules/Header/Header";
+
+const Cart = () => {
+  return (
+    <>
+      <Header />
+      <main></main>
+      <Footer />
+    </>
+  );
+};
+
+export default Cart;
